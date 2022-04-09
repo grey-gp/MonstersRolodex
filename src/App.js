8dc8657833951ef1698d24e0194b292dce87c186
grey-gp <grey.grahamp@gmail.com>
2022-04-09 11:45:16 -0400

    Changed monsters state to be fetched from json API instead of hard-coded values

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import { Component } from "react";
+import { Component } from 'react';
 
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: 'Graham',
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name}</p>
-          <button onClick={()=>{
-            this.setState(() => {return {name: 'Yihua'}})
-          }}>Change Name</button>
-        </header>
+      <div className='App'>
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
